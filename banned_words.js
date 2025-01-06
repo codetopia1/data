fabbe36dbde48906c767226219980ea936e195f1
codetopia1 <mahdi.greenleaf@gmail.com>
2025-01-07 00:37:11 +0330

    Create banned_words.js

---
+++ banned_words.js
@@ -0,0 +1,12 @@
+const bannedWords = [
+    "admin",
+    "root",
+    "superuser",
+    "password",
+    "123456",
+    "qwerty",
+    "abc123",
+    "بایگانی",  // کلمات فارسی
+    "مدیر",
+    "سیستم"
+];
