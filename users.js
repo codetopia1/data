7ba48b4f7f0a86c3773e13371705b7cd06472c9e
codetopia1 <mahdi.greenleaf@gmail.com>
2025-01-07 00:09:53 +0330

    Update users.js

--- users.js
+++ users.js
@@ -3,10 +3,5 @@ const users = [
     "username": "Mahdi.Mlk",
     "password": "474",
     "time": "2025-01-06T20:39:23.982Z"
-  },
-  {
-    "username": "mahdi.mlk",
-    "password": "sdfg",
-    "time": "2025-01-06T20:39:31.158Z"
   }
 ];
