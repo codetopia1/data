5399e9b1f4235528f70c6834fd08289e760bf360
codetopia1 <mahdi.greenleaf@gmail.com>
2025-01-07 10:48:59 +0330

    Updated file

--- users.js
+++ users.js
@@ -18,5 +18,10 @@ const users = [
     "username": "نام کاربری",
     "password": 1234,
     "time": "2025-01-07T07:08:55.456Z"
+  },
+  {
+    "username": "مدیر",
+    "password": 1234,
+    "time": "2025-01-07T07:19:00.794Z"
   }
 ];
