1469969bbecc0dd7fd0314fb61c3f0e795a0aee5
codetopia1 <mahdi.greenleaf@gmail.com>
2025-01-27 01:56:49 +0330

    Update scores.js

--- scores.js
+++ scores.js
@@ -1,90 +1,34 @@
 const scores = [
   {
-    "username": "mahdi",
+    "username": "user1",
     "stars": "0"
   },
   {
-    "username": "mahd1i",
-    "stars": 100
+    "username": "user2",
+    "stars": 1
   },
   {
-    "username": "mahdi1",
-    "stars": 100
+    "username": "user3",
+    "stars": 1
   },
   {
-    "username": "mahdi2",
-    "stars": 200
+    "username": "user4",
+    "stars": 1
   },
   {
-    "username": "mahdi3",
-    "stars": 300
+    "username": "user5",
+    "stars": 1
   },
   {
-    "username": "mahdi4",
-    "stars": 400
+    "username": "user6",
+    "stars": 1
   },
   {
-    "username": "mahdi5",
-    "stars": 500
+    "username": "user7",
+    "stars": 1
   },
   {
-    "username": "mahdi6",
-    "stars": 600
-  },
-  {
-    "username": "mahdi7",
-    "stars": 700
-  },
-  {
-    "username": "mahdi8",
-    "stars": 800
-  },
-  {
-    "username": "mahdi9",
-    "stars": 1200
-  },
-  {
-    "username": "mahdi20",
-    "stars": 100
-  },
-  {
-    "username": "mahdi21",
-    "stars": 250
-  },
-  {
-    "username": "mahdi22",
-    "stars": 501
-  },
-  {
-    "username": "مهدی 123",
-    "stars": 310
-  },
-  {
-    "username": "fhf",
-    "stars": 310
-  },
-  {
-    "username": "مهدی ملکان",
-    "stars": 313
-  },
-  {
-    "username": "طیبایبا",
-    "stars": 310
-  },
-  {
-    "username": "Parisa",
-    "stars": 3
-  },
-  {
-    "username": "Hgff",
-    "stars": 3
-  },
-  {
-    "username": "",
-    "stars": 3
-  },
-  {
-    "username": "فاطمه ",
-    "stars": "0"
+    "username": "user8",
+    "stars": 1
   }
 ];
